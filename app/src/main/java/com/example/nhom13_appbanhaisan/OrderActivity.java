d0566d0057356e36612301c3f4cde8b10bf269ee
Hồ Bá Đông <120149583+dong123333@users.noreply.github.com>
2023-10-24 10:33:44 +0700

    Create README.md

--- app/src/main/java/com/example/nhom13_appbanhaisan/OrderActivity.java
+++ app/src/main/java/com/example/nhom13_appbanhaisan/OrderActivity.java
@@ -1,14 +1,13 @@
 package com.example.nhom13_appbanhaisan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class OrderActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_order);
     }
 }
