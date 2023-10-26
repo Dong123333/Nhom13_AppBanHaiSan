a1ec213e37c4e314ffad61fac99798b0a5453b94
Dong123333 <hobadong777@gmail.com>
2023-10-26 18:57:10 +0700

    Initial commit

--- app/src/main/java/com/example/nhom13_appbanhaisan/SplashScreenActivity.java
+++ app/src/main/java/com/example/nhom13_appbanhaisan/SplashScreenActivity.java
@@ -2,13 +2,9 @@ package com.example.nhom13_appbanhaisan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-=======
-import android.os.Bundle;
->>>>>>> origin/codeGiaoDien/Dong123333
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -16,7 +12,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-<<<<<<< HEAD
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -25,7 +20,5 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         },3000);
-=======
->>>>>>> origin/codeGiaoDien/Dong123333
     }
 }
