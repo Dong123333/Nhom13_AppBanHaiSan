f6c8e463b9b668f8765a10faf3ffa1d4a637b176
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2023-10-27 22:45:58 +0700

    update page setting

--- app/src/main/java/com/example/nhom13_appbanhaisan/AboutUsActivity.java
+++ app/src/main/java/com/example/nhom13_appbanhaisan/AboutUsActivity.java
@@ -1,14 +1,26 @@
 package com.example.nhom13_appbanhaisan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUsActivity extends AppCompatActivity {
+
+    private ImageView backAboutUs = findViewById(R.id.back_aboutus);
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+    backAboutUs.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
