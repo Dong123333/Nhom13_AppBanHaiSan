0eb8dc94e55349d13783457e49ec28ea681dccc6
toancyto09 <101265301+toancyto09@users.noreply.github.com>
2023-10-27 22:01:56 +0700

    update page setting

---
+++ app/src/main/java/com/example/nhom13_appbanhaisan/SettingActivity.java
@@ -0,0 +1,40 @@
+package com.example.nhom13_appbanhaisan;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private ImageView arrowback;
+    private TextView aboutus;
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        arrowback = (ImageView) findViewById(R.id.btnback);
+        aboutus = (TextView) findViewById(R.id.thanhviennhom);
+
+        arrowback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PersonalInfoManagementActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PersonalInfoManagementActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
